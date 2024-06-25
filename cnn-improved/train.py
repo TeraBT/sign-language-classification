424fbf77427a4a9e2bd9c16a2ea28947cf5fc269
Chris <chris.baumgartner115@gmail.com>
2024-06-25 21:35:02 +0200

    CNN submission-ready.

--- cnn-improved/train.py
+++ cnn-improved/train.py
@@ -2,6 +2,4 @@ import utils.training_algorithm
 from utils.sign_language_models import ImprovedModel
 from utils.transformers import improved_transformer
 
-
-# utils.training_algorithm.train('cnn-improved.pth', ImprovedModel, 20, improved_transformer)
-utils.training_algorithm.train('cnn-improved.pth', ImprovedModel, 20, None)
+utils.training_algorithm.train('cnn-improved.pth', ImprovedModel, 20, improved_transformer)
